d6435688bd4f6f55d8cc104546d627224acfd1c9
Diggo 🤙🗿🤙 <125403873+RgoSL@users.noreply.github.com>
2024-06-13 23:20:15 -0300

    Add files via upload

---
+++ Exercicios Java/TarefasJava/src/main/java/javaintroducao/Tarefas3Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaintroducao;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Tarefas3Java {
+    public static void main(String[] args) {
+     double B , h , A;
+     B=6;
+     h=7;
+     A=B*h;
+        System.out.println("A área do retangulo é "+A);
+    }
+    
+}
